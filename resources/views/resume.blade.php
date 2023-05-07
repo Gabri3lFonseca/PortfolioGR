9aae3c16458c6d5b6190e99dc8b3da76a0c62419
Gabriel <gafonsecaaa@gmail.com>
2023-05-07 09:07:39 -0300

    Primeiro commit

---
+++ resources/views/resume.blade.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Resume - Gabriel Fonseca - Portfolio</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Kelly
+  * Updated: Mar 10 2023 with Bootstrap v5.2.3
+  * Template URL: https://bootstrapmade.com/kelly-free-bootstrap-cv-resume-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container-fluid d-flex justify-content-between align-items-center">
+
+      <h1 class="logo me-auto me-lg-0"><a href="index">Gabriel</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a href="home">Home</a></li>
+          <li><a href="about">About</a></li>
+          <li><a class="active" href="resume">Resume</a></li>
+          <li><a href="contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <div class="header-social-links">
+        <a href="https://www.instagram.com/gabrielfonseca_13/" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="https://www.linkedin.com/in/gabriel-fonseca-bb8281275/" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+      </div>
+
+    </div>
+
+  </header><!-- End Header -->
+
+  <main id="main">
+
+    <!-- ======= Resume Section ======= -->
+    <section id="resume" class="resume">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Resume</h2>
+          <p>A seguir um resumo sobre minha trajetória educacional, desde a educação infantil até a conclusão da minha formação acadêmica, destacando as instituições onde estudei.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6">
+            <h3 class="resume-title">Ensino Fundamental I</h3>
+            <div class="resume-item">
+              <h4>Escola Pingo de Gente</h4>
+              <h5>2013 - 2016</h5>
+              <p><em>O Escola Pingo De Gente oferece toda a estrutura necessária para o conforto e desenvolvimento educacional dos seus alunos, como por exemplo: Banda Larga, Parque Infantil, Biblioteca, Pátio Descoberto, Área Verde e Internet.</em></p>
+              <p>
+              <ul>
+                <li>R Gleudes Terra De Oliveira, 95- Sagrado Coracao De Jesus, Formiga - Minas Gerais</li>
+                <li>(37) 3322-1457</li>
+                <li>escolapingodegente@bol.com.br</li>
+              </ul>
+              </p>
+            </div>
+
+            <h3 class="resume-title">Ensino Fundamental II</h3>
+            <div class="resume-item">
+              <h4>UNIFOR-MG</h4>
+              <h5>2017 - 2020</h5>
+              <p><em>O Centro Universitário de Formiga – UNIFOR-MG, equiparando-se às grandes instituições de ensino superior do país, conta com mais de 55 modernos laboratórios, os quais permitem que os alunos coloquem em prática o que eles aprendem na sala de aula.</em></p>
+              <p>
+              <ul>
+                <li>https://uniformg.edu.br</li>
+              </ul>
+              </p>
+            </div>
+            
+          </div>
+          <div class="col-lg-6">
+            <h3 class="resume-title">Ensino Médio</h3>
+            <div class="resume-item">
+              <h4>IFMG - Formiga</h4>
+              <h5>2021 - Present</h5>
+              <p><em>O IFMG – Campus Formiga é uma instituição pública federal que tem como objetivo oferecer uma educação gratuita de qualidade, buscando o desenvolvimento social, tecnológico e econômico do país. O campus tem em seu corpo docente professores altamente qualificados  com títulos de mestrado e doutorado e uma equipe administrativa e pedagógica capacitada a conduzir o aluno ao sucesso profissional.</em></p>
+              <p>
+              <ul>
+                <li>R. São Luiz Gonzaga, s/n - Bairro São Luiz, Formiga - MG</li>
+                <li>(37) 3322-8434</li>
+                <li>https://formiga.ifmg.edu.br</li>
+              </ul>
+              </p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Resume Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Gabriel Fonseca</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/kelly-free-bootstrap-cv-resume-html-template/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- End  Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
